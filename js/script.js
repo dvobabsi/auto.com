a618b5eb0ec308e72de62c6be597921ac1a91648
dvobabsi <dvobabsi@gmail.com>
2024-05-22 15:55:49 +0300

    fix: edit social links

--- js/script.js
+++ js/script.js
@@ -7,3 +7,51 @@ $(document).ready(function() {
         $(this).toggleClass('active').next().slideToggle(300);
     });
 });
+
+//burger
+
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+}); 
+
+
+let isMobile = {
+	Android: function() {return navigator.userAgent.match(/Android/i);},
+	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
+		let body=document.querySelector('body');
+if(isMobile.any()){
+		body.classList.add('touch');
+		let arrow = document.querySelectorAll('.arrow');
+	for(i=0; i<arrow.length; i++) {
+			let thisLink = arrow[i].previousElementSibling;
+			let subMenu = arrow[i].nextElementSibling;
+			let thisArrow = arrow[i];
+			
+			thisLink.classList.add('parent');
+		arrow[i].addEventListener('click', function(){
+			subMenu.classList.toggle('open');
+			thisArrow.classList.toggle('active');
+		});
+	}
+}else{
+	body.classList.add('mouse');
+}
+
+//---
+
+document.addEventListener('DOMContentLoaded', function () {
+    new ChiefSlider('.slider', {
+        loop: true,
+        autoplay: true,
+        interval: 6000,
+        refresh: true,
+    });
+});
